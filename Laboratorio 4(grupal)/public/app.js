5cf6051954a73706e1794e06b13064d86aa78663
SrEstrada <sestradaa@unsa.edu.pe>
2025-05-06 21:43:00 -0500

    Crear función saveFile para guardar archivos mediante POST

--- Laboratorio 4(grupal)/public/app.js
+++ Laboratorio 4(grupal)/public/app.js
@@ -6,4 +6,13 @@ function loadFile(name) {
         document.getElementById('file-content').innerHTML = html;
       });
   }
+function saveFile() {
+    const name = document.getElementById('new-name').value;
+    const content = document.getElementById('new-content').value;
   
+    fetch('/api/file', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, content })
+    });
+  }
