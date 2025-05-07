c2bbd311a4ad3c27cd6f3dea6f8f12afa98f116b
Renzo Geomar Mamani Quispe <renzomamaniq@unsa.edu.pe>
2025-05-06 21:22:33 -0500

    Listar archivos Markdown

--- Laboratorio 4(grupal)/server.js
+++ Laboratorio 4(grupal)/server.js
@@ -9,3 +9,12 @@ const MARKDOWN_DIR = path.join(__dirname, 'markdowns');
 
 app.use(express.static('public'));
 app.use(express.json());
+
+// Listar archivos Markdown
+app.get('/api/files', (req, res) => {
+    fs.readdir(MARKDOWN_DIR, (err, files) => {
+      if (err) return res.status(500).json({ error: 'Error al leer directorio' });
+      const mdFiles = files.filter(file => file.endsWith('.md'));
+      res.json(mdFiles);
+    });
+  });
